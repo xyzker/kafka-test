aadd84919b4faa9134bcf934e2742c661a609fa8
xyzker <410317669@qq.com>
2017-04-13 14:13:30 +0800

    add

---
+++ src/main/java/stream/KafkaStream.java
@@ -0,0 +1,26 @@
+package stream;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStreamBuilder;
+
+public class KafkaStream {
+    public static void main(String[] args) {
+        Map<String, Object> props = new HashMap<>();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "my-stream-processing-application");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "10.110.25.196:9093,10.110.25.197:9093,10.110.25.198:9093");
+        props.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        StreamsConfig config = new StreamsConfig(props);
+
+        KStreamBuilder builder = new KStreamBuilder();
+        builder.stream("my-input-topic").mapValues(value -> value.getClass().toString()).to("my-output-topic");
+
+        KafkaStreams streams = new KafkaStreams(builder, config);
+        streams.start();
+    }
+}
